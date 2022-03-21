2c78d3462afa9db1c15c8a7ebbf4d275adf9e38f
Preethi Panneerselvam <preethi.panneerselvam@resmed.com>
2022-03-21 13:31:52 -0600

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+# Public Cloud Configuration
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test123"
+  secret_key                  = "testabc"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_use_path_style           = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+# Create Bucket
+resource "aws_s3_bucket" "b" {
+  bucket = "test-bucket"
+}
+
+# Upload an object
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.b.id
+  key    = "test.txt"
+  acl    = "public-read"
+  source = "test.txt"
+}
